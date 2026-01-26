3b6e0338e3c948fc1608337eb1f625376389700c
Uday12704 <uday12704@gmail.com>
2026-01-26 15:51:47 +0530

    add custom JWT session claims and update auth middleware for role-based access control

--- apps/order-service/src/middleware/authMiddleware.ts
+++ apps/order-service/src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { getAuth } from "@clerk/fastify";
 import { FastifyReply, FastifyRequest } from "fastify";
+import type { CustomJwtSessionClaims } from "@repo/types"
 
 declare module "fastify" {
     interface FastifyRequest {
@@ -8,10 +9,23 @@ declare module "fastify" {
 }
 
 export const shouldBeUser = async (request: FastifyRequest, reply: FastifyReply) => {
-    const { userId } = getAuth(request)
-    if(!userId) {
-        return reply.send({message: "You are not logged in!"})
+    const auth = getAuth(request)
+    if(!auth.userId) {
+        return reply.status(401).send({message: "You are not logged in!"})
     }
 
-    request.userId = userId;
+    request.userId = auth.userId;
+}
+export const shouldBeAdmin = async (request: FastifyRequest, reply: FastifyReply) => {
+    const auth = getAuth(request)
+    if(!auth.userId) {
+        return reply.status(401).send({message: "You are not logged in!"})
+    }
+
+    const claims = auth.sessionClaims as CustomJwtSessionClaims
+    if(claims.metadata?.role !== "admin"){
+        return reply.status(403).send({message: "Unauthorized!"})
+    }
+
+    request.userId = auth.userId;
 }
--- apps/product-service/src/middleware/authMiddleware.ts
+++ apps/product-service/src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { getAuth } from '@clerk/express';
 import express, { Request, Response, NextFunction } from 'express';
+import { CustomJwtSessionClaims } from "@repo/types"
 
 declare global {
     namespace Express {
@@ -21,6 +22,29 @@ export const shouldBeUser = (
         return res.status(401).json({
             message: "Yor are not logged in!"
         })
+    }
+
+    req.userId = auth.userId;
+    return next();
+}
+
+export const shouldBeAdmin = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const auth = getAuth(req);
+    const userId = auth.userId;
+
+    if(!userId) {
+        return res.status(401).json({
+            message: "Yor are not logged in!"
+        })
+    }
+
+    const claims = auth.sessionClaims as CustomJwtSessionClaims
+    if(claims.metadata?.role !== "admin") {
+        return res.status(403).send({message: "Unauthorized!"})
     }
 
     req.userId = auth.userId;
